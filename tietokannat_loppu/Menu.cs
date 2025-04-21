344b3ea85eb463d7146d99bb20df4f0957fc3b9f
Mikko Asp <bmias002@edu.xamk.fi>
2025-04-21 12:19:54 +0300

    Added login to menu
    -Logging in is now mandatory and a new user is not made every time the program is run
    -If user doesnt log in they have to create a new account before they can add recipes to the db
    -Code refactoring

--- tietokannat_loppu/Menu.cs
+++ tietokannat_loppu/Menu.cs
@@ -1,69 +1,118 @@
-﻿using tietokannat_loppu.Entities;
+﻿using CrypticWizard.RandomWordGenerator;
+using Microsoft.EntityFrameworkCore;
+using tietokannat_loppu.Entities;
+using static CrypticWizard.RandomWordGenerator.WordGenerator;
 
 namespace BaseConsoleApp
 {
+    //Default login account used:
+    //uglyvaintoad@hotmail.com
+    //mxg595i85f36w21f75a0
     internal class Menu
     {
-        public async Task StartMenu(LocalUser user)
+        IAskDetails helper = new Helper();
+        IDatabaseHandler dbHandler;
+        RecipeManager recipeHandler;
+        TietokannatLoppuContext context = new();
+        bool running = true;
+        LocalUser user;
+
+        string[] possibleMenuOptions = { "1. Add a new recipe", "2. Show all recipes", "3. Update a recipe", "4. Delete A Recipe With Id", "5. Search For Recipes By Ingredients", "6. Search For Recipes By Dish", "7. Search For Recipes Based On Diet", "8. End Program", "9. Debug: SaveToDb" };
+        public Menu()
         {
-            bool running = true;
-            TietokannatLoppuContext context = new();
-            IDatabaseHandler handler = new DatabaseManager(context);
-            IAskDetails helper = new Helper();
+            dbHandler = new DatabaseManager(context);
+            recipeHandler = new RecipeHandlingManager(dbHandler, helper);
+        }
 
-            RecipeManager recipeHandling = new RecipeHandlingManager(handler, helper);
-
-
-            await handler.AddUserToDb(user);
+        public async Task StartMenu()
+        {
+            user = AskLoginOptions().Result;
+            string loginDetails = $"Login as:{user.UserName}\nEmail:{user.Email}\n";
             while (running)
             {
-                switch (PrintMenuOptions(user))
+                MenuOption menuOption = (MenuOption)PrintMenuOptions(possibleMenuOptions, loginDetails);
+                switch (menuOption)
                 {
-                    case 0:
-                        await recipeHandling.AddNewRecipe(user);
+                    case MenuOption.AddRecipe:
+                        await recipeHandler.AddNewRecipe(user);
                         break;
-                    case 1:
-                        recipeHandling.ShowAllRecipes();
+                    case MenuOption.ShowAllRecipes:
+                        recipeHandler.ShowAllRecipes();
                         break;
-                    case 2:
-                        recipeHandling.UpdateRecipe();
+                    case MenuOption.UpdateRecipe:
+                        recipeHandler.UpdateRecipe();
                         break;
-                    case 3:
-                        recipeHandling.DeleteRecipeWithId();
+                    case MenuOption.DeleteRecipe:
+                        recipeHandler.DeleteRecipeWithId();
                         break;
-                    case 4:
-                        recipeHandling.PrintRecipe(recipeHandling.SearchRecipesByIngredients(helper.AskRecipeIngredients()));
+                    case MenuOption.SearchWithIngredients:
+                        recipeHandler.PrintRecipe(recipeHandler.SearchRecipesByIngredients(helper.AskRecipeIngredients()));
                         break;
-                    case 5:
-                        recipeHandling.SearchRecipesByDishes();
+                    case MenuOption.SearchWithDish:
+                        recipeHandler.SearchRecipesByDishes();
                         break;
-                    case 6:
-                        recipeHandling.SearchRecipesByDiets();
+                    case MenuOption.SearchWithDiet:
+                        recipeHandler.SearchRecipesByDiets();
                         break;
-                    case 7:
+                    case MenuOption.EndProgram:
+                        Console.WriteLine("Program ended");
                         running = false;
                         break;
-                    case 8:
-                        await handler.SaveRecipesToDatabaseAsync(new Localrecipe("DebugTest", Dish.Main, new List<string> {"DebugIngredient 1", "DebugIngredient 2"}, new List<string> {"Step 1", "Step 2"}, Diet.Meat), user);
+                    case MenuOption.DebugSaveToDb:
+                        await dbHandler.SaveRecipesToDatabaseAsync(new Localrecipe("DebugTest", Dish.Main, new List<string> {"DebugIngredient 1", "DebugIngredient 2"}, new List<string> {"Step 1", "Step 2"}, Diet.Meat), user);
                         Console.WriteLine("DONE");
                         break;
+
                 }
                 Console.WriteLine("Press any key to continue.");
                 Console.ReadLine();
             }
 
         }
-        private int PrintMenuOptions(LocalUser user)
+        private async Task<LocalUser> AskLoginOptions()
         {
-            string[] options = { "1. Add a new recipe", "2. Show all recipes", "3. Update a recipe" ,"4. Delete A Recipe With Id", "5. Search For Recipes By Ingredients", "6. Search For Recipes By Dish", "7. Search For Recipes Based On Diet","8. End Program", "9. Debug: SaveToDb" };
+            bool loggingIn = true;
+            LocalUser? user;
+            while (loggingIn)
+            {
+                Console.WriteLine("Create a new user or login to existing one with email and password");
+                string[] loginOptions = { "Create new user", "Login to Existing user","Debug: login to default account" };
+                LoginOption option = (LoginOption)PrintMenuOptions(loginOptions, "Login to existing user with email and password or create a new user.\n");
+
+                if (option == LoginOption.Login)
+                {
+                    user = CreateRandomUser();
+                    await dbHandler.AddUserToDb(user);
+                    return user;
+                }
+                else if(option == LoginOption.CreateUser)
+                {
+                    string email = helper.AskString("Enter email: ");
+                    string password = helper.AskString("Enter password: ");
+                    user = LoginUser(email, password);
+
+                    if(user != null)
+                    {
+                        return user;
+                    }
+                }
+                else if(option == LoginOption.DebugLogin)
+                {
+                    return LoginUser("uglyvaintoad@hotmail.com", "mxg595i85f36w21f75a0");
+                }
+            }
+            throw new Exception("Unexpected error during login.");
+        }
+        private int PrintMenuOptions(string[] options, string? msg = null)
+        {
             int currentIndex = 0;
+            int optionsLength = options.Length;
             ConsoleKey keyPressed;
             do
             {
                 Console.Clear();
-                Console.WriteLine("Username: " + user.UserName);
-                Console.WriteLine("Email: " + user.Email);
-                Console.WriteLine("\nSelect Function You Want To Use (Use Arrow Keys, Numbers 1-8, and Enter):");
+                Console.WriteLine(msg);
+                Console.WriteLine($"Select your option with Arrow Keys, Numbers 1-{optionsLength}, and Enter");
 
                 for (int i = 0; i < options.Length; i++)
                 {
@@ -79,7 +128,7 @@ namespace BaseConsoleApp
 
                 keyPressed = Console.ReadKey(true).Key;
 
-                if (keyPressed >= ConsoleKey.D1 && keyPressed <= ConsoleKey.D8)
+                if (keyPressed >= ConsoleKey.D1 && keyPressed <= ConsoleKey.D1 + optionsLength - 1)
                 {
                     currentIndex = keyPressed - ConsoleKey.D1;
                 }
@@ -98,6 +147,88 @@ namespace BaseConsoleApp
             }
             while (keyPressed != ConsoleKey.Enter);
             return currentIndex;
+        }
+        private LocalUser? LoginUser(string email, string password)
+        {
+            var user = context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+            if(user != null)
+            {
+                Console.WriteLine("Login success");
+                Console.ReadLine();
+                return new LocalUser(user.Username, user.Email, user.Password);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect login credentials");
+                Console.ReadLine();
+                return null;
+            }
+        }
+        private LocalUser CreateRandomUser()
+        {
+            WordGenerator generator = new WordGenerator();
+            string start = generator.GetWord(PartOfSpeech.adj);
+            string middle = generator.GetWord(PartOfSpeech.adj);
+            string end = generator.GetWord(PartOfSpeech.noun);
+
+            string userName = $"{start} {middle} {end}";
+
+            string[] options = { "@gmail.com", "@outlook.com", "@yahoo.com", "@hotmail.com", "@icloud.com", "@aol.com", "@zoho.com", "@protonmail.com" };
+
+            Random rand = new Random();
+
+            int value = rand.Next(0, options.Length);
+
+            string emailOption = options[value];
+
+            string email = start + middle + end + emailOption;
+
+            string password = GeneratePassword(rand);
+
+
+            return new LocalUser(userName, email, password);
+        }
+        private string GeneratePassword(Random rand)
+        {
+            string result = "";
+            // Generate 20 characters (letters or numbers)
+            for (int i = 0; i < 20; i++)
+            {
+                // Decide randomly whether to add a letter or a number
+                if (rand.Next(0, 2) == 0) // 50% chance to add a letter
+                {
+                    // Generate a random letter from 'a' to 'z'
+                    char letter = (char)('a' + rand.Next(0, 26));
+                    result += letter;
+                }
+                else
+                {
+                    // Generate a random number between 0 and 9
+                    char number = (char)('0' + rand.Next(0, 10));
+                    result += number;
+                }
+            }
+
+            return result;
+        }
+
+        private enum LoginOption
+        {
+            Login = 0,
+            CreateUser = 1,
+            DebugLogin = 2
+        }
+        private enum MenuOption
+        {
+            AddRecipe = 0,
+            ShowAllRecipes = 1,
+            UpdateRecipe = 2,
+            DeleteRecipe = 3,
+            SearchWithIngredients = 4,
+            SearchWithDish = 5,
+            SearchWithDiet = 6,
+            EndProgram = 7,
+            DebugSaveToDb = 8
         }
     }
 }
--- tietokannat_loppu/Program.cs
+++ tietokannat_loppu/Program.cs
@@ -10,57 +10,10 @@ namespace BaseConsoleApp
         static async Task Main(string[] args)
         {
             Menu menu = new();
-            await menu.StartMenu(CreateRandomUser());
-
-        }
-
-        static LocalUser CreateRandomUser()
-        {
-            WordGenerator generator = new WordGenerator();
-            string start = generator.GetWord(PartOfSpeech.adj);
-            string middle = generator.GetWord(PartOfSpeech.adj);
-            string end = generator.GetWord(PartOfSpeech.noun);
-
-            string userName = $"{start} {middle} {end}";
-
-            string[] options = { "@gmail.com", "@outlook.com", "@yahoo.com", "@hotmail.com", "@icloud.com", "@aol.com", "@zoho.com", "@protonmail.com" };
-
-            Random rand = new Random();
-
-            int value = rand.Next(0, options.Length);
-
-            string emailOption = options[value];
-
-            string email = start + middle + end + emailOption;
-
-            string password = GeneratePassword(rand);
 
 
-            return new LocalUser(userName, email, password);
-        }
+            await menu.StartMenu();
 
-        static string GeneratePassword(Random rand)
-        {
-            string result = "";
-            // Generate 20 characters (letters or numbers)
-            for (int i = 0; i < 20; i++)
-            {
-                // Decide randomly whether to add a letter or a number
-                if (rand.Next(0, 2) == 0) // 50% chance to add a letter
-                {
-                    // Generate a random letter from 'a' to 'z'
-                    char letter = (char)('a' + rand.Next(0, 26));
-                    result += letter;
-                }
-                else
-                {
-                    // Generate a random number between 0 and 9
-                    char number = (char)('0' + rand.Next(0, 10));
-                    result += number;
-                }
-            }
-
-            return result;
         }
     }
 }
